155ad6fa9a1a5c28ba1f2d5593bd15d0ab08c0a9
suhail-09 <you@example.com>
2024-01-19 15:09:22 -0800

    #0x0A. Configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#this file creates a File in /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet\n',
+}
